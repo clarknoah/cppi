11dcad84d815039df8932561ba18373d8f6f880c
Noah Clark <noahbc08@gmail.com>
2016-10-21 10:56:55 -0400

    added questions

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 SUBJECT_ROOT="/Volumes/coax_lab/patrick_project/subjects/"
 FREESURFER_SUBJECT_ROOT="/Volumes/coax_lab/patrick_project/freesurfer"
-for subject in
-echo "Hello, world $SUBJECT_ROOT!"
+printf "Hello, please select which areas your would like (use spaces between Numbers) \n"
+printf "Area 1: 1 \n"
+printf "Area 1: 1 \n"
+printf "Area 1: 1 \n"
+printf "Area 1: 1 \n"
+printf "Area 1: 1 \n"
+printf "Area 1: 1 \n"
+printf "Area 1: 1 \n"
+
+read NAME
+echo $NAME
+for subject in 0006 0012 0273 0192 0490 0491 0269 0464 0494 0495 0549 0550 0557 0558 0559 0604 0605 0627
+do
+  echo $SUBJECT_ROOT${subject}
+done
