db0c44918eb7297f0042a9affe69b9711e1f6f97
Noah Clark <noahbc08@gmail.com>
2016-10-21 16:07:50 -0400

    closes 24, closes 9, closes 17, closes 5

---
+++ source/cppi_utils/cppi_generate_difference.m
@@ -0,0 +1,7 @@
+function [difference] = cppi_generate_difference(value1, value2)
+%CPPI_GENERATE_DIFFERENCE Returns the difference of two values
+%   Detailed explanation goes here
+    difference = value1 - value2;
+
+end
+
