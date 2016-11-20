68ce4d4db2000d77f5c242d39a1f97b1a812a588
Noah Clark <noahbc08@gmail.com>
2016-11-20 01:53:03 -0500

    little touchups

--- scripts/test/test_cppi_batch_subjects.m
+++ scripts/test/test_cppi_batch_subjects.m
@@ -3,8 +3,9 @@ addpath('~/Documents/MATLAB/spm12');
 addpath('~/Documents/MATLAB/NIfTI_20140122');
 addpath('~/Documents/MATLAB/jsonlab');
 
-
+here = pwd;
 config = loadjson('config/config.json');
 payload = loadjson('data/payload.json');
-config.subjects = [config.subjects(1)];                  
+config.subjects = [config.subjects(1),config.subjects(2)];                  
 tools = cppi_batch_subjects(config,payload);
+cd(pwd);
